850bd26d8d404ba7d08b50ac4d486fc47c7473ce
David Hurtado <davidhuertado@gmail.com>
2025-01-08 08:08:56 +0100

    feat home

--- src/index.js
+++ src/index.js
@@ -5,3 +5,33 @@ toggleMobileMenu.addEventListener('click', () => {
   mobileMenu.classList.toggle('!left-0');
   toggleMobileMenu.classList.toggle('!bg-tan');
 });
+
+const swiper = new Swiper('.swiper-mobile', {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+const swiperDesktop = new Swiper('.swiper-desktop', {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  let yPosition = window.scrollY;
+  console.log(yPosition);
+  if (yPosition > 1) {
+    header.style.backgroundColor = 'white';
+  } else {
+    header.style.backgroundColor = 'transparent';
+  }
+});
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    fontSize: {
+      sm: '16px',
+      base: '20px',
+      xl: '24px',
+      '2xl': '30px',
+      '3xl': '40px',
+      '4xl': '50px',
+    },
+    fontFamily: {
+      hattom: ['Hatton', 'sans-serif'],
+      josefin: ['Josefin Sans', 'sans-serif'],
+      sans: ['Hatton', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        tan: '#c88d76',
+        'tan-light': '#BAA195',
+        beige: '#EEE9E4',
+        'gray-dark': '#61534B',
+        'gray-light': '#726660',
+        white: '#ffffff',
+      },
+    },
+  },
+  plugins: [],
+};
